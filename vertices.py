17f88ae556e8bb45036e029d94f80ddd3131c00c
saad-mh <saad.ans2018@gmail.com>
2026-02-20 11:50:57 +0530

    vertuxes again

--- vertices.py
+++ vertices.py
@@ -7,7 +7,7 @@ if n > 3:
     G = nx.complete_graph(n)
     pos = nx.spring_layout(G)
     nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray')
-    plt.title(f'Complete Graph with {n} Vertices')
+    plt.title(f'Graph with {n} Vertices')
     plt.show()
 else:
     print("Please enter a number greater than 3 to create a complete graph.")
