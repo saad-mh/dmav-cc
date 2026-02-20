bdf1fd00d8a08425025383979b63a223748822f8
saad-mh <saad.ans2018@gmail.com>
2026-02-20 10:31:09 +0530

    vertuxes

---
+++ vertices.py
@@ -0,0 +1,13 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+n = int(input("Enter the number of vertices: "))
+
+if n > 3:
+    G = nx.complete_graph(n)
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray')
+    plt.title(f'Complete Graph with {n} Vertices')
+    plt.show()
+else:
+    print("Please enter a number greater than 3 to create a complete graph.")
